270f4b55a9d1a44158f788b15c72c88b8eb83c89
Hackathon <akumar193@sapient.com>
2015-02-28 02:47:05 +0530

    no message

--- HealthUtilityHackathon/SlideMenu/Helper Classes/HomeViewController.h
+++ HealthUtilityHackathon/SlideMenu/Helper Classes/HomeViewController.h
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 #import "SlideNavigationController.h"
+#import "XYPieChart.h"
+@interface HomeViewController : UIViewController <SlideNavigationControllerDelegate,XYPieChartDelegate, XYPieChartDataSource>
+@property (weak, nonatomic) IBOutlet XYPieChart *protineView;
 
-@interface HomeViewController : UIViewController <SlideNavigationControllerDelegate>
-
+@property (weak, nonatomic) IBOutlet XYPieChart *calorieIntakeView;
 @property (nonatomic, strong) IBOutlet UISwitch *limitPanGestureSwitch;
 @property (nonatomic, strong) IBOutlet UISwitch *slideOutAnimationSwitch;
 @property (nonatomic, strong) IBOutlet UISwitch *shadowSwitch;
@@ -27,5 +29,11 @@
 - (IBAction)enablePanGestureSelected:(id)sender;
 - (IBAction)portraitSlideOffsetChanged:(id)sender;
 - (IBAction)landscapeSlideOffsetChanged:(id)sender;
+@property (weak, nonatomic) IBOutlet XYPieChart *carboHydrateView;
+@property (weak, nonatomic) IBOutlet XYPieChart *fatView;
+@property (weak, nonatomic) IBOutlet XYPieChart *burnCalorieView;
+
+@property(nonatomic, strong) NSMutableArray *slices;
+@property(nonatomic, strong) NSArray        *sliceColors;
 
 @end
--- HealthUtilityHackathon/SlideMenu/Helper Classes/HomeViewController.m
+++ HealthUtilityHackathon/SlideMenu/Helper Classes/HomeViewController.m
@@ -11,6 +11,15 @@
 
 @implementation HomeViewController
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.calorieIntakeView reloadData];
+    [self.fatView reloadData];
+    [self.protineView reloadData];
+    [self.carboHydrateView reloadData];
+    [self.burnCalorieView reloadData];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -22,6 +31,98 @@
 	self.shadowSwitch.on = [SlideNavigationController sharedInstance].enableShadow;
 	self.limitPanGestureSwitch.on = ([SlideNavigationController sharedInstance].panGestureSideOffset == 0) ? NO : YES;
 	self.slideOutAnimationSwitch.on = ((LeftMenuViewController *)[SlideNavigationController sharedInstance].leftMenu).slideOutAnimationEnabled;
+    
+    ///
+    self.slices = [NSMutableArray arrayWithCapacity:2];
+    
+    for(int i = 0; i < 2; i ++)
+    {
+        NSNumber *one = [NSNumber numberWithInt:rand()%60+20];
+        [_slices addObject:one];
+    }
+   
+    
+    
+    [self.calorieIntakeView setDataSource:self];
+    [self.calorieIntakeView setDelegate:self];
+    [self.calorieIntakeView setStartPieAngle:M_PI_2];
+    [self.calorieIntakeView setAnimationSpeed:1.0];
+    [self.calorieIntakeView setLabelFont:[UIFont fontWithName:@"DBLCDTempBlack" size:24]];
+    [self.calorieIntakeView setLabelRadius:40];
+    [self.calorieIntakeView setShowPercentage:YES];
+    [self.calorieIntakeView setPieBackgroundColor:[UIColor colorWithWhite:0.95 alpha:1]];
+    [self.calorieIntakeView setPieCenter:CGPointMake(75, 75)];
+    [self.calorieIntakeView setUserInteractionEnabled:NO];
+    [self.calorieIntakeView setLabelShadowColor:[UIColor blackColor]];
+    
+    [self.fatView setDataSource:self];
+    [self.fatView setStartPieAngle:M_PI_2];
+    [self.fatView setAnimationSpeed:1.0];
+    [self.fatView setLabelFont:[UIFont fontWithName:@"DBLCDTempBlack" size:12]];
+    [self.fatView setLabelRadius:20];
+    [self.fatView setShowPercentage:YES];
+    [self.fatView setPieBackgroundColor:[UIColor colorWithWhite:0.95 alpha:1]];
+    [self.fatView setPieCenter:CGPointMake(43, 43)];
+    [self.fatView setUserInteractionEnabled:NO];
+    [self.fatView setLabelShadowColor:[UIColor blackColor]];
+    
+    [self.carboHydrateView setDataSource:self];
+    [self.carboHydrateView setStartPieAngle:M_PI_2];
+    [self.carboHydrateView setAnimationSpeed:1.0];
+    [self.carboHydrateView setLabelFont:[UIFont fontWithName:@"DBLCDTempBlack" size:12]];
+    [self.carboHydrateView setLabelRadius:20];
+    [self.carboHydrateView setShowPercentage:YES];
+    [self.carboHydrateView setPieBackgroundColor:[UIColor colorWithWhite:0.95 alpha:1]];
+    [self.carboHydrateView setPieCenter:CGPointMake(43, 43)];
+    [self.carboHydrateView setUserInteractionEnabled:NO];
+    [self.carboHydrateView setLabelShadowColor:[UIColor blackColor]];
+    
+    [self.protineView setDataSource:self];
+    [self.protineView setStartPieAngle:M_PI_2];
+    [self.protineView setAnimationSpeed:1.0];
+    [self.protineView setLabelFont:[UIFont fontWithName:@"DBLCDTempBlack" size:12]];
+    [self.protineView setLabelRadius:20];
+    [self.protineView setShowPercentage:YES];
+    [self.protineView setPieBackgroundColor:[UIColor colorWithWhite:0.95 alpha:1]];
+    [self.protineView setPieCenter:CGPointMake(43, 43)];
+    [self.protineView setUserInteractionEnabled:NO];
+    [self.protineView setLabelShadowColor:[UIColor blackColor]];
+    
+    [self.burnCalorieView setDataSource:self];
+    [self.burnCalorieView setStartPieAngle:M_PI_2];
+    [self.burnCalorieView setAnimationSpeed:1.0];
+    [self.burnCalorieView setLabelFont:[UIFont fontWithName:@"DBLCDTempBlack" size:24]];
+    [self.burnCalorieView setLabelRadius:40];
+    [self.burnCalorieView setShowPercentage:YES];
+    [self.burnCalorieView setPieBackgroundColor:[UIColor colorWithWhite:0.95 alpha:1]];
+    [self.burnCalorieView setPieCenter:CGPointMake(75, 75)];
+    [self.burnCalorieView setUserInteractionEnabled:NO];
+    [self.burnCalorieView setLabelShadowColor:[UIColor blackColor]];
+    
+    self.sliceColors =[NSArray arrayWithObjects:
+                       [UIColor colorWithRed:246/255.0 green:155/255.0 blue:0/255.0 alpha:1],
+                       [UIColor colorWithRed:129/255.0 green:195/255.0 blue:29/255.0 alpha:1],
+                       [UIColor colorWithRed:62/255.0 green:173/255.0 blue:219/255.0 alpha:1],
+                       [UIColor colorWithRed:229/255.0 green:66/255.0 blue:115/255.0 alpha:1],
+                       [UIColor colorWithRed:148/255.0 green:141/255.0 blue:139/255.0 alpha:1],nil];
+    
+//    [self.pieChartRight setDelegate:self];
+//    [self.pieChartRight setDataSource:self];
+//    [self.pieChartRight setPieCenter:CGPointMake(240, 240)];
+//    [self.pieChartRight setShowPercentage:NO];
+//    [self.pieChartRight setLabelColor:[UIColor blackColor]];
+//    
+//    [self.percentageLabel.layer setCornerRadius:90];
+//    
+//    self.sliceColors =[NSArray arrayWithObjects:
+//                       [UIColor colorWithRed:246/255.0 green:155/255.0 blue:0/255.0 alpha:1],
+//                       [UIColor colorWithRed:129/255.0 green:195/255.0 blue:29/255.0 alpha:1],
+//                       [UIColor colorWithRed:62/255.0 green:173/255.0 blue:219/255.0 alpha:1],
+//                       [UIColor colorWithRed:229/255.0 green:66/255.0 blue:115/255.0 alpha:1],
+//                       [UIColor colorWithRed:148/255.0 green:141/255.0 blue:139/255.0 alpha:1],nil];
+//    
+//    //rotate up arrow
+//    self.downArrow.transform = CGAffineTransformMakeRotation(M_PI);
 }
 
 #pragma mark - SlideNavigationController Methods -
@@ -111,5 +212,43 @@
 			return 0;
 	}
 }
+
+#pragma mark - XYPieChart Data Source
+
+- (NSUInteger)numberOfSlicesInPieChart:(XYPieChart *)pieChart
+{
+    return self.slices.count;
+}
+
+- (CGFloat)pieChart:(XYPieChart *)pieChart valueForSliceAtIndex:(NSUInteger)index
+{
+    return [[self.slices objectAtIndex:index] intValue];
+}
+
+- (UIColor *)pieChart:(XYPieChart *)pieChart colorForSliceAtIndex:(NSUInteger)index
+{
+    //if(pieChart == self.pieChartRight) return nil;
+    return [self.sliceColors objectAtIndex:(index % self.sliceColors.count)];
+}
+
+#pragma mark - XYPieChart Delegate
+- (void)pieChart:(XYPieChart *)pieChart willSelectSliceAtIndex:(NSUInteger)index
+{
+    NSLog(@"will select slice at index %d",index);
+}
+- (void)pieChart:(XYPieChart *)pieChart willDeselectSliceAtIndex:(NSUInteger)index
+{
+    NSLog(@"will deselect slice at index %d",index);
+}
+- (void)pieChart:(XYPieChart *)pieChart didDeselectSliceAtIndex:(NSUInteger)index
+{
+    NSLog(@"did deselect slice at index %d",index);
+}
+- (void)pieChart:(XYPieChart *)pieChart didSelectSliceAtIndex:(NSUInteger)index
+{
+    NSLog(@"did select slice at index %d",index);
+    //self.selectedSliceLabel.text = [NSString stringWithFormat:@"$%@",[self.slices objectAtIndex:index]];
+}
+
 
 @end
