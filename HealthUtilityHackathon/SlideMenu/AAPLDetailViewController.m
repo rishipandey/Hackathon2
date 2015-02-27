cd60eb25020e32600039f004a5ac841ec7f6c5dc
Hackathon <akumar193@sapient.com>
2015-02-28 01:51:58 +0530

    no message

--- HealthUtilityHackathon/SlideMenu/AAPLDetailViewController.m
+++ HealthUtilityHackathon/SlideMenu/AAPLDetailViewController.m
@@ -90,64 +90,81 @@ Abstract:
 }
 
 #pragma mark - Table View
-
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 50;
+}
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-	return 2;
+	return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-	if (section == 0) {
-		return [(AAPLMotionActivityQuery *)_detailItem isToday] ? 5 : 4;
-	} else if (section == 1) {
-		return 1;
-	} else if (section == 2){
-		return _activityDataManager.significantActivitiesAndSteps.count;
-	} else {
-		return 0;
-	}
+    return 1;
+//	if (section == 1) {
+//		return [(AAPLMotionActivityQuery *)_detailItem isToday] ? 5 : 4;
+//	} else if (section == 0) {
+//		return 1;
+//	}
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     cell.detailTextLabel.textColor = [UIColor greenColor];
-	if (indexPath.section == 0) {
+	if (indexPath.section == 1) {
 		if (indexPath.row == 0) {
-			cell.textLabel.text = @"Walking";
-			cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ --%0.1f kCal",[AAPLDetailViewController formatTimeInterval:_activityDataManager.walkingDuration],(_activityDataManager.walkingDuration/60)*1];
-		} else if (indexPath.row == 1) {
-			cell.textLabel.text = @"Running";
-			cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ --%0.1f kCal",[AAPLDetailViewController formatTimeInterval:_activityDataManager.runningDuration],(_activityDataManager.walkingDuration/60)*1.8];
-		} else if (indexPath.row == 2) {
-			cell.textLabel.text = @"Driving";
-			cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ --%0.1f kCal",[AAPLDetailViewController formatTimeInterval:_activityDataManager.vehicularDuration],(_activityDataManager.walkingDuration/60)*0.25];;
-		} else if (indexPath.row == 3) {
-			cell.textLabel.text = @"Moving";
-			cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ --%0.1f kCal",[AAPLDetailViewController formatTimeInterval:_activityDataManager.movingDuration],(_activityDataManager.walkingDuration/60)*0.5];;;
-		} else if (indexPath.row == 4) {
-			cell.textLabel.text = @"Current activity";
-			cell.detailTextLabel.text = _currentActivity;
-		}
-	} else if (indexPath.section == 1) {
+            cell.textLabel.hidden = YES;
+            cell.detailTextLabel.hidden = YES;
+            
+//			cell.textLabel.text = @"Calories:";
+//            cell.textLabel.frame = CGRectMake(0, 10, 100, 20);
+//			cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ --%0.1f kCal",[AAPLDetailViewController formatTimeInterval:_activityDataManager.walkingDuration],(_activityDataManager.walkingDuration/60)*1];
+
+            UILabel *lblDuration = [[UILabel alloc] initWithFrame:CGRectMake(220, 10, 70, 20)];
+            [cell.contentView addSubview:lblDuration];
+            lblDuration.text = [AAPLDetailViewController formatTimeInterval:_activityDataManager.walkingDuration];
+            UILabel *lblCalories = [[UILabel alloc] initWithFrame:CGRectMake(220, 35, 70, 20)];
+            [cell.contentView addSubview:lblCalories];
+            lblCalories.text = [NSString stringWithFormat:@"%0.1f kCal",(_activityDataManager.walkingDuration/60)*1];
+            UILabel *lblDurationVal = [[UILabel alloc] initWithFrame:CGRectMake(20, 10, 70, 20)];
+            [cell.contentView addSubview:lblDurationVal];
+            lblDurationVal.text = @"Duration:";
+            UILabel *lblCaloriesVal = [[UILabel alloc] initWithFrame:CGRectMake(20, 35, 70, 20)];
+            [cell.contentView addSubview:lblCaloriesVal];
+            lblCaloriesVal.text = @"Calories:";
+		} //else if (indexPath.row == 1) {
+//			cell.textLabel.text = @"Running";
+//			cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ --%0.1f kCal",[AAPLDetailViewController formatTimeInterval:_activityDataManager.runningDuration],(_activityDataManager.walkingDuration/60)*1.8];
+//		}
+	} else if (indexPath.section == 0) {
+        
 		cell.textLabel.text = [(AAPLMotionActivityQuery *)_detailItem isToday] ? @"Live Step Counts" : @"Step Counts";
 		cell.detailTextLabel.text = [NSString stringWithFormat:@"%ld",
 									[_activityDataManager.stepCounts longValue] + _currentSteps];
-	} else if (indexPath.section == 2) {
-
-		AAPLSignificantActivity *activity = _activityDataManager.significantActivitiesAndSteps[indexPath.row];
-		cell.textLabel.text = [NSString stringWithFormat:@"%@ (%@ - %@)",
-			[AAPLActivityDataManager activityTypeToString:activity.activityType],
-			[_dateFormatter stringFromDate:activity.startDate],
-			[_dateFormatter stringFromDate:activity.endDate]];
-		if (activity.activityType == ActivityTypeWalking || activity.activityType == ActivityTypeRunning) {
-			cell.detailTextLabel.text = [activity.stepCounts description];
-		}
-		else {
-			cell.detailTextLabel.text = @"n/a";
-		}
+        cell.detailTextLabel.textColor = [UIColor blackColor];
 	}
+    else if (indexPath.section == 2) {
+        if (indexPath.row == 0) {
+            cell.textLabel.hidden = YES;
+            cell.detailTextLabel.hidden = YES;
+            UILabel *lblDuration = [[UILabel alloc] initWithFrame:CGRectMake(220, 10, 70, 20)];
+            [cell.contentView addSubview:lblDuration];
+            lblDuration.text = [AAPLDetailViewController formatTimeInterval:_activityDataManager.runningDuration];
+            UILabel *lblCalories = [[UILabel alloc] initWithFrame:CGRectMake(220, 35, 70, 20)];
+            [cell.contentView addSubview:lblCalories];
+            lblCalories.text = [NSString stringWithFormat:@"%0.1f  kCal",(_activityDataManager.runningDuration/60)*1];
+            UILabel *lblDurationVal = [[UILabel alloc] initWithFrame:CGRectMake(20, 10, 70, 20)];
+            [cell.contentView addSubview:lblDurationVal];
+            lblDurationVal.text = @"Duration:";
+            UILabel *lblCaloriesVal = [[UILabel alloc] initWithFrame:CGRectMake(20, 35, 70, 20)];
+            [cell.contentView addSubview:lblCaloriesVal];
+            lblCaloriesVal.text = @"Calories:";
+//            cell.textLabel.text = @"Calories:";
+//            cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ --%0.1f kCal",[AAPLDetailViewController formatTimeInterval:_activityDataManager.runningDuration],(_activityDataManager.walkingDuration/60)*1.8];
+        }
+    }
 
 	return cell;
 }
@@ -155,19 +172,31 @@ Abstract:
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
 	switch (section) {
-		case 0:
-			return @"Activity";
 		case 1:
+			return @"Walking";
+		case 0:
 			return @"Pedometer";
-		case 2:
-			return @"Filtered History";
+        case 2:
+            return @"Running";
+
 	}
 	return @"";
 }
 
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.section == 0)
+    {
+        return 44.0f;
+    }
+    else{
+        return 66;
+    }
+}
+
 + (NSString *)formatTimeInterval:(NSTimeInterval)interval
 {
-	return [NSString stringWithFormat:@"%2uh %2um",
+	return [NSString stringWithFormat:@"%2uh   %2um",
 		(int)(interval / 3600),
 		(int)(interval / 60) % 60];
 }
