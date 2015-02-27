bd46053528ba932abef937f9172ee14176708064
Rishi Pandey 2 <rpandey14@sapient.com>
2015-02-27 22:30:30 +0530

    v1.0

---
+++ HealthUtility_v1.0/HealthUtility/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthUtility
+//
+//  Created by Ratul Chhibber on 26/02/15.
+//  Copyright (c) 2015 Ratul Chhibber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HealthUtility_v1.0/HealthUtility/SidebarTableViewController.m
@@ -0,0 +1,67 @@
+//
+//  SidebarTableViewController.m
+//  SidebarDemo
+//
+//  Created by Simon Ng on 10/11/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+#import "SidebarTableViewController.h"
+#import "SWRevealViewController.h"
+
+@interface SidebarTableViewController ()
+
+@end
+
+@implementation SidebarTableViewController {
+    NSArray *menuItems;
+    UINavigationController *destViewController;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    menuItems = @[@"title",@"home", @"nutrition", @"healthTracker", @"lipidBurner"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return menuItems.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *CellIdentifier = [menuItems objectAtIndex:indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    return cell;
+}
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    // Set the title of navigation bar by using the menu items
+    NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+    destViewController = (UINavigationController*)segue.destinationViewController;
+    destViewController.title = [[menuItems objectAtIndex:indexPath.row] capitalizedString];
+    
+}
+
+
+
+@end
