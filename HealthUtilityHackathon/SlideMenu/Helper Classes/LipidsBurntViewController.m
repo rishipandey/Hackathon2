afdb67dfef7c8bcb35431d5823e712d172bd5e46
Rishi Pandey 2 <rpandey14@sapient.com>
2015-02-28 08:20:44 +0530

    Bar chart fixes.

--- HealthUtilityHackathon/SlideMenu/Helper Classes/LipidsBurntViewController.m
+++ HealthUtilityHackathon/SlideMenu/Helper Classes/LipidsBurntViewController.m
@@ -31,7 +31,7 @@
     [super viewWillAppear:animated];
     
     OVGraphView *graphview=[[OVGraphView alloc]initWithFrame:CGRectMake(0, 70, self.view.frame.size.width, 400) ContentSize:CGSizeMake(960, 300)];
-    graphview.plotContainer.reverse=YES;
+    graphview.plotContainer.reverse=NO;
     graphview.plotContainer.graphcolor=[UIColor colorWithRed:0.31 green:0.73 blue:0.78 alpha:1.0];
     [self.view addSubview:graphview];
     
@@ -50,16 +50,16 @@
     OVGraphViewPoint *thirtennthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"1 AM" YValue:@9 ];
     OVGraphViewPoint *fouthteenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"00 AM" YValue:@8 ];
     
-    OVGraphViewPoint *fifteenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"2 PM" YValue:@8 ];
-    OVGraphViewPoint *sixteenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"3 PM" YValue:@8 ];
-    OVGraphViewPoint *seventeenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"4 PM" YValue:@8 ];
-    OVGraphViewPoint *eighteenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"5 PM" YValue:@8 ];
-    OVGraphViewPoint *nineteenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"6 PM" YValue:@8 ];
-    OVGraphViewPoint *twentyPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"7 PM" YValue:@8 ];
-    OVGraphViewPoint *twentyonePoint = [[OVGraphViewPoint alloc]initWithXLabel:@"8 PM" YValue:@8 ];
-    OVGraphViewPoint *twentytwoPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"9 PM" YValue:@8 ];
-    OVGraphViewPoint *twentythrePoint = [[OVGraphViewPoint alloc]initWithXLabel:@"10 PM" YValue:@8 ];
-    OVGraphViewPoint *twentyfourPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"11 PM" YValue:@8 ];
+    OVGraphViewPoint *fifteenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"2 PM" YValue:@50 ];
+    OVGraphViewPoint *sixteenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"3 PM" YValue:@23 ];
+    OVGraphViewPoint *seventeenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"4 PM" YValue:@4 ];
+    OVGraphViewPoint *eighteenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"5 PM" YValue:@35 ];
+    OVGraphViewPoint *nineteenthPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"6 PM" YValue:@1 ];
+    OVGraphViewPoint *twentyPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"7 PM" YValue:@20 ];
+    OVGraphViewPoint *twentyonePoint = [[OVGraphViewPoint alloc]initWithXLabel:@"8 PM" YValue:@10 ];
+    OVGraphViewPoint *twentytwoPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"9 PM" YValue:@100 ];
+    OVGraphViewPoint *twentythrePoint = [[OVGraphViewPoint alloc]initWithXLabel:@"10 PM" YValue:@1 ];
+    OVGraphViewPoint *twentyfourPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"11 PM" YValue:@0 ];
     
     //OVGraphViewPoint *boundaryPoint = [[OVGraphViewPoint alloc]initWithXLabel:@"" YValue:nil ];
     
